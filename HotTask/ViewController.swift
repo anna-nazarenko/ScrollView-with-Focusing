8b11e1d2f2d3fa93221c089bbb1f4e3aeaf22204
Anna.Nazarenko <anna.nazarenko@pettersonapps.com>
2022-10-18 13:53:34 +0300

    Made deceleration faster, replaced UITextView by UILabel, and changed targetOffset

--- HotTask/ViewController.swift
+++ HotTask/ViewController.swift
@@ -11,13 +11,14 @@ class ViewController: UIViewController {
   
   //MARK: Properties
   
-  private var targetOffset: Int = 0
+  private var targetOffsetY: Int = 0
   private var lastContentOffsetY: CGFloat = 0
   
   //MARK: UI Elements
   
   lazy var scrollView: UIScrollView = {
     let scrollView = UIScrollView()
+    scrollView.decelerationRate = UIScrollView.DecelerationRate.fast
     scrollView.translatesAutoresizingMaskIntoConstraints = false
     return scrollView
   }()
@@ -28,10 +29,12 @@ class ViewController: UIViewController {
       return contentView
   }()
   
-  private lazy var subview1: UITextView = {
-    let subview1 = UITextView()
+  private lazy var subview1: UILabel = {
+    let subview1 = UILabel()
     subview1.text = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
     subview1.font = UIFont.systemFont(ofSize: 22)
+    subview1.numberOfLines = 0
+    subview1.lineBreakMode = .byWordWrapping
     subview1.translatesAutoresizingMaskIntoConstraints = false
     return subview1
   }()
@@ -108,18 +111,16 @@ class ViewController: UIViewController {
 extension ViewController: UIScrollViewDelegate {
   func scrollViewDidScroll(_ scrollView: UIScrollView) {
     let contentOffsetY = scrollView.contentOffset.y
+    let contentHeight = scrollView.contentSize.height
     let scrollHeight = scrollView.frame.size.height
     
-    let heightOfSubview1 = subview1.bounds.height
-    let heightOfSubview2 = subview2.bounds.height
-    
-    if lastContentOffsetY < contentOffsetY { targetOffset = Int(heightOfSubview1 + heightOfSubview2 / 2  - (scrollHeight / 2)) }
-    else { targetOffset = Int((heightOfSubview1 / 2) - (scrollHeight / 2)) }
+    if lastContentOffsetY < contentOffsetY { targetOffsetY = Int(contentHeight - scrollHeight)}
+    else { targetOffsetY = 0 }
     
     lastContentOffsetY = contentOffsetY
   }
   
   func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-    targetContentOffset.pointee = CGPoint(x: 0, y: self.targetOffset)
+    targetContentOffset.pointee = CGPoint(x: 0, y: self.targetOffsetY)
   }
 }
