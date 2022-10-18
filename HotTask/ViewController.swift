85d9e93f94f57cee9101f45b2de5a1ecaaafd330
Anna.Nazarenko <anna.nazarenko@pettersonapps.com>
2022-10-18 13:23:21 +0300

    Initial commit

--- HotTask/ViewController.swift
+++ HotTask/ViewController.swift
@@ -8,12 +8,118 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  //MARK: Properties
+  
+  private var targetOffset: Int = 0
+  private var lastContentOffsetY: CGFloat = 0
+  
+  //MARK: UI Elements
+  
+  lazy var scrollView: UIScrollView = {
+    let scrollView = UIScrollView()
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    return scrollView
+  }()
+  
+  private lazy var contentView: UIView = {
+      let contentView = UIView()
+      contentView.translatesAutoresizingMaskIntoConstraints = false
+      return contentView
+  }()
+  
+  private lazy var subview1: UITextView = {
+    let subview1 = UITextView()
+    subview1.text = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+    subview1.font = UIFont.systemFont(ofSize: 22)
+    subview1.translatesAutoresizingMaskIntoConstraints = false
+    return subview1
+  }()
+  
+  private lazy var subview2: UIImageView = {
+    let subview2 = UIImageView()
+    subview2.backgroundColor = .yellow
+    subview2.contentMode = .scaleAspectFit
+    subview2.image = UIImage(named: "icon")
+    subview2.translatesAutoresizingMaskIntoConstraints = false
+    return subview2
+  }()
+  
+  //MARK: View Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    scrollView.delegate = self
+    
+    view.addSubview(scrollView)
+    scrollView.addSubview(contentView)
+    contentView.addSubview(subview1)
+    contentView.addSubview(subview2)
+    
+    setScrollViewConstraints()
+    setContentViewConstraints()
+    setTextViewConstraints()
+    setGreenViewConstraints()
   }
-
-
+  
+  //MARK: Constraints
+  
+  private func setScrollViewConstraints() {
+      NSLayoutConstraint.activate([
+          scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+          scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+          scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+          scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+      ])
+  }
+  
+  private func setContentViewConstraints() {
+      NSLayoutConstraint.activate([
+          contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+          contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+          contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+          contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+          contentView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+          contentView.heightAnchor.constraint(equalToConstant: 1600)
+      ])
+  }
+  
+  private func setTextViewConstraints() {
+      NSLayoutConstraint.activate([
+        subview1.topAnchor.constraint(equalTo: contentView.topAnchor),
+        subview1.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+        subview1.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+        subview1.heightAnchor.constraint(equalToConstant: 800),
+      ])
+  }
+  
+  private func setGreenViewConstraints() {
+      NSLayoutConstraint.activate([
+        subview2.topAnchor.constraint(equalTo: subview1.bottomAnchor),
+        subview2.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+        subview2.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+        subview2.heightAnchor.constraint(equalToConstant: 800),
+      ])
+  }
 }
 
+//MARK: - ScrollView Delegate
+
+extension ViewController: UIScrollViewDelegate {
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    let contentOffsetY = scrollView.contentOffset.y
+    let scrollHeight = scrollView.frame.size.height
+    
+    let heightOfSubview1 = subview1.bounds.height
+    let heightOfSubview2 = subview2.bounds.height
+    
+    if lastContentOffsetY < contentOffsetY { targetOffset = Int(heightOfSubview1 + heightOfSubview2 / 2  - (scrollHeight / 2)) }
+    else { targetOffset = Int((heightOfSubview1 / 2) - (scrollHeight / 2)) }
+    
+    lastContentOffsetY = contentOffsetY
+  }
+  
+  func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+    targetContentOffset.pointee = CGPoint(x: 0, y: self.targetOffset)
+  }
+}
